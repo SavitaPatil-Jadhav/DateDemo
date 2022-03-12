5918b268e948fc83cff7297bb73b1231e1aa7537
SavitaPatil-Jadhav <savitapatil1091@gmail.com>
2022-03-12 12:55:52 +0530

    First Commit

---
+++ src/MyDate.java
@@ -0,0 +1,26 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyDate {
+
+	void show(Date d) {
+		
+		System.out.println("Date :"+d);
+		
+		int day = d.getDay();
+		int month = d.getMonth();
+		int year = d.getYear();
+		
+		String date = day+"/"+month+"/"+year;
+		
+System.out.println(date);
+		
+	}
+	public static void main(String[] args) {
+		
+		MyDate md = new MyDate();
+		
+		md.show(new Date(2015,8,9));
+		
+	}
+}
